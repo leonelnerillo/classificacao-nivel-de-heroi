f04550c7a8cf994bca93985e82f5268c0288eb73
leonerillo <leonel.nerillo@hotmail.com>
2024-10-13 02:10:16 -0300

    Primeiro Commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// variaveis com nome e xp do herói e exibição do nivel
+let nomeDoHeroi = "Perseu";
+let xpDoHeroi = 12500;
+let nivelDoHeroi;
+
+if (xpDoHeroi < 1000){
+    nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 6000){
+    nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+    nivelDoHeroi = "Platina";
+} else if (xpDoHeroi >+ 7001 && xpDoHeroi <+ 8000) {
+    nivelDoHeroi = "Diamante";
+} else if (xpDoHeroi >+ 8001 && xpDoHeroi <+ 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi >+ 9001 && xpDoHeroi <+ 10000) {
+    nivelDoHeroi = "Imortal";
+} else if (xpDoHeroi >= 10001) {
+    nivelDoHeroi = "Radiante";
+}
+
+console.log("O Herói " + nomeDoHeroi + ", está no nivel " + nivelDoHeroi);
